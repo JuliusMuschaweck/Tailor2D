f595a5061f2904db9533abae0664a96e015dbc51
Julius Muschaweck <julius@zs66.de>
2020-08-23 16:22:13 +0200

    VectorialSnell.pdf

--- TestVectorialSnell.m
+++ TestVectorialSnell.m
@@ -3,7 +3,7 @@ ni = sqrt(2)-1e-15;
 no = 1;
 n = [1;0];
 in = ni * Unit2D([1;1]);
-out = in + (sqrt(no^2 - ni^2 + in' * n) - in' * n) * n
+out = in + (sqrt(no^2 - ni^2 + (in' * n)^2) - in' * n) * n
 a_in = Cross2D(Unit2D(in),n);
 a_out = Cross2D(Unit2D(out),n);
 a_in * ni - a_out * no
